c895c262a895e4f01b05926b0e32549764aa1995
Sai Kiran Jella <jsaikiran19@gmail.com>
2023-08-29 00:46:35 -0400

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { DarkMode, LightMode } from "@mui/icons-material";
 import DownloadIcon from "@mui/icons-material/Download";
-import { Backdrop, IconButton, Menu, MenuItem, useMediaQuery } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Backdrop,
+  IconButton,
+  Menu,
+  MenuItem,
+  useMediaQuery
+} from "@mui/material";
+import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
-import { createTheme } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 import "./App.scss";
 import resume from "./assets/Sai_Kiran_Jella_Resume.pdf";
 import Experience from "./components/experience/experience";
 import Projects from "./components/projects/projects";
 import Skills from "./components/skills/skills";
-import MenuIcon from '@mui/icons-material/Menu';
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 
+export const ThemeContext = createContext('light');
 function CircularProgressWithLabel(props) {
   return (
     <Box sx={{ position: "relative", display: "inline-flex" }}>
@@ -48,28 +56,12 @@ CircularProgressWithLabel.propTypes = {
    */
   value: PropTypes.number.isRequired,
 };
-
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        light: "#757ce8",
-        main: "#3f50b5",
-        dark: "#002884",
-        contrastText: "#fff",
-      },
-      secondary: {
-        light: "#ff7961",
-        main: "#f44336",
-        dark: "#ba000d",
-        contrastText: "#000",
-      },
-    },
-  });
   const isMobile = useMediaQuery("(max-width: 768px)");
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [progress, setProgress] = React.useState(1);
   const [open, setOpen] = useState(false);
+  const [theme, setTheme] = useState('light');
   React.useEffect(() => {
     const timer = setInterval(() => {
       setProgress((prev) => (prev >= 100 ? 0 : prev + 10));
@@ -81,7 +73,8 @@ function App() {
     }, 1100);
   }, []);
   function preventNavigation(e) {
-    const el = e.target.hash || e.target.attributes.getNamedItem('href').nodeValue;
+    const el =
+      e.target.hash || e.target.attributes.getNamedItem("href").nodeValue;
     e.preventDefault();
     document.getElementById(el.slice(1)).scrollIntoView();
     setOpen(false);
@@ -91,9 +84,29 @@ function App() {
   }
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
-    setOpen(true)
+    setOpen(true);
   };
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main:'#fff'
+      },
+      background: '#282c34'
+    },
+  });
+  const lightTheme = createTheme({
+    palette: {
+      mode:'light',
+      primary: {
+        main: '#3f50b5',
+        dark: '#002884',
+      },
+      background:'white'
+    }
+  });
   return (
+    <ThemeProvider theme={theme==='light' ? lightTheme : darkTheme}>
     <div className="App">
       {progress < 100 ? (
         <Backdrop
@@ -103,58 +116,76 @@ function App() {
           <CircularProgressWithLabel value={progress} />
         </Backdrop>
       ) : (
-        <React.Fragment>
+        <div className={theme}>
           <header className="app-header">
             <div className="left-pane"></div>
             <div className="right-pane">
-              { isMobile ? <><IconButton
-                aria-label="more"
-                id="long-button"
-                aria-controls={open ? "long-menu" : undefined}
-                aria-expanded={open ? "true" : undefined}
-                aria-haspopup="true"
-                onClick={handleClick}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu open={open} onClose={() => setOpen(false)} anchorEl={anchorEl}>
-                <MenuItem href="#projects" onClick={preventNavigation}>
-                  Projects
-                </MenuItem>
-                <MenuItem href="#experience" onClick={preventNavigation}>
-                  Experience
-                </MenuItem>
-                <MenuItem href="#skills" onClick={preventNavigation}>
-                  Skills
-                </MenuItem>
-                <MenuItem download href={resume} onClick={preventNavigation}>
-                  Resume
-                </MenuItem>
-              </Menu></> : <nav>
-                <a href="#projects" onClick={preventNavigation}>
-                  Projects
-                </a>
-                <a href="#experience" onClick={preventNavigation}>
-                  Experience
-                </a>
-                <a href="#skills" onClick={preventNavigation}>
-                  Skills
-                </a>
-                {/* <a href="#contact" onClick={preventNavigation}>
+              {isMobile ? (
+                <>
+                  <IconButton
+                    aria-label="more"
+                    id="long-button"
+                    aria-controls={open ? "long-menu" : undefined}
+                    aria-expanded={open ? "true" : undefined}
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Menu
+                    open={open}
+                    onClose={() => setOpen(false)}
+                    anchorEl={anchorEl}
+                  >
+                    <MenuItem href="#projects" onClick={preventNavigation}>
+                      Projects
+                    </MenuItem>
+                    <MenuItem href="#experience" onClick={preventNavigation}>
+                      Experience
+                    </MenuItem>
+                    <MenuItem href="#skills" onClick={preventNavigation}>
+                      Skills
+                    </MenuItem>
+                    <MenuItem
+                      download
+                      href={resume}
+                      onClick={preventNavigation}
+                    >
+                      Resume
+                    </MenuItem>
+                  </Menu>
+                </>
+              ) : (
+                <nav>
+                  <a href="#projects" onClick={preventNavigation}>
+                    Projects
+                  </a>
+                  <a href="#experience" onClick={preventNavigation}>
+                    Experience
+                  </a>
+                  <a href="#skills" onClick={preventNavigation}>
+                    Skills
+                  </a>
+                  {/* <a href="#contact" onClick={preventNavigation}>
                   Contact
                 </a> */}
-                <a download href={resume}>
-                  Resume
-                </a>
-              </nav>}
-              
+                  <a download href={resume}>
+                    Resume
+                  </a>
+                  <IconButton onClick={()=>setTheme(theme==='light' ? 'dark': 'light')}>{theme==='light' ? 
+                  <LightMode sx={{background: '#3f50b5', color:'white', borderRadius:'2rem', padding:'0.3rem' }}/> :
+                   <DarkMode sx={{color:'white', background:'black', borderRadius:'2rem', padding:'0.3rem'}}/>}</IconButton>
+                </nav>
+              )}
             </div>
           </header>
           <div className="welcome-page">
             <div className="greeting">Hi, This is Sai Kiran Jella</div>
             <div className="description">
-              A passionate Full Stack Developer with three years of experience
-              and a Masters in CS from Indiana University Bloomington.
+              A passionate Full Stack Developer with a three-year background and
+              a Master's degree in Computer Science from Indiana University
+              Bloomington. My expertise involves building web applications
+              utilizing Angular, React, Spring Boot, and ExpressJS.
             </div>
             <div className="socials">
               <a
@@ -213,9 +244,10 @@ function App() {
             <div className="section-title">Contact</div>
 
           </section> */}
-        </React.Fragment>
+        </div>
       )}
     </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -7,68 +7,75 @@ import "react-vertical-timeline-component/style.min.css";
 import iu from "../../assets/IU.jpeg";
 import adp from "../../assets/adp.jpg";
 import mgit from "../../assets/mgit.png";
-import './experience.scss';
+import "./experience.scss";
+import { useTheme } from "@mui/material";
 
 const Icon = (props) => (
-  <img
-    src={props.path}
-    style={{ borderRadius: 40 }}
-    alt=""
-  />
+  <img src={props.path} style={{ borderRadius: 40 }} alt="" />
 );
 export default function Experience() {
   const experiences = [
     {
-      date:'May 2022 - May 2023',
-      position:'left',
-      title: 'Application Developer',
-      description:'Designed web applications using Angular, Ngxs, Storybook, and Figma. Automated the deployment process using Github actions to deploy the application on Netlify under 5 minutes.',
-      path: iu
+      date: "May 2022 - May 2023",
+      position: "left",
+      title: "Application Developer",
+      description:
+        "Designed web applications using Angular, Ngxs, Storybook, and Figma. Automated the deployment process using Github actions to deploy the application on Netlify under 5 minutes.",
+      path: iu,
     },
     {
-      date: 'Aug 2021 - May 2023',
-      position:'right',
-      title:'Masters in Computer Science',
-      description:'Graduated with a Masters in CS at Indiana University Bloomington.',
-      path: iu
+      date: "Aug 2021 - May 2023",
+      position: "right",
+      title: "Masters in Computer Science",
+      description:
+        "Graduated with a Masters in CS at Indiana University Bloomington.",
+      path: iu,
     },
     {
-      date: 'Aug 2019 - Aug 2021',
-      position:'left',
-      title: 'Full Stack Engineer',
-      description:'Revamped a Jenkins pipeline and utilized Docker to reduce build times by 20 minutes. Slashed the initial load size of the application by 80\% by migrating AngularJS to Angular 8 and implementing a lazy loading approach. Spearheaded an initiative to establish a unified user experience across multiple applications, ensuring a seamless and intuitive interaction for users',
-      path: adp
+      date: "Aug 2019 - Aug 2021",
+      position: "left",
+      title: "Full Stack Engineer",
+      description:
+        "Revamped a Jenkins pipeline and utilized Docker to reduce build times by 20 minutes. Slashed the initial load size of the application by 80% by migrating AngularJS to Angular 8 and implementing a lazy loading approach. Spearheaded an initiative to establish a unified user experience across multiple applications, ensuring a seamless and intuitive interaction for users",
+      path: adp,
     },
     {
-      date: 'Aug 2015 - May 2019',
-      position:'right',
-      title: 'Bachelor in Technology',
-      description:'Pursued a bachelor in Computer Science at Mahatma Gandhi Institute of Technology',
-      path: mgit
-    }
-  ]
+      date: "Aug 2015 - May 2019",
+      position: "right",
+      title: "Bachelor in Technology",
+      description:
+        "Pursued a bachelor in Computer Science at Mahatma Gandhi Institute of Technology",
+      path: mgit,
+    },
+  ];
+  const theme = useTheme().palette.mode;
   return (
     <VerticalTimeline>
-      {experiences && experiences.map(exp=>{
-        return(
-          <VerticalTimelineElement
-          key={exp.title}
-        date={exp.date}
-        position={exp.position}
-        icon={<Icon path={exp.path}></Icon>}
-        contentArrowStyle={{ borderRight: "7px solid  lightblue" }}
-        contentStyle={{ background: "white", boxShadow: "0 0 5px 2px #ddd"}}
-        iconStyle={{
-          height: "5rem",
-          width: "5rem",
-          boxShadow:
-            "0 0 0 4px grey, inset 0 2px 0 rgba(0,0,0,.08), 0 3px 0 4px rgba(0,0,0,.05)",
-        }}
-      >
-        <h3 className="exp-title">{exp.title}</h3>
-        <div className="exp-desc">{exp.description}</div>
-      </VerticalTimelineElement>
-      )})}
+      {experiences &&
+        experiences.map((exp) => {
+          return (
+            <VerticalTimelineElement
+              key={exp.title}
+              date={exp.date}
+              position={exp.position}
+              icon={<Icon path={exp.path}></Icon>}
+              contentArrowStyle={{ borderRight: "7px solid  lightblue" }}
+              contentStyle={{
+                background: theme==='light' ? "white": "#282c34",
+                boxShadow: "inset 0 0 0.5px 1px hsla(0, 0%, 100%, 0.075), 0 0 0 1px hsla(0, 0%, 0%, 0.05), 0 0.3px 0.4px hsla(0, 0%, 0%, 0.02), 0 0.9px 1.5px hsla(0, 0%, 0%, 0.045), 0 3.5px 6px hsla(0, 0%, 0%, 0.09)",
+              }}
+              iconStyle={{
+                height: "5rem",
+                width: "5rem",
+                boxShadow:
+                  "0 0 0 4px grey, inset 0 2px 0 rgba(0,0,0,.08), 0 3px 0 4px rgba(0,0,0,.05)",
+              }}
+            >
+              <h3 className="exp-title">{exp.title}</h3>
+              <div className="exp-desc">{exp.description}</div>
+            </VerticalTimelineElement>
+          );
+        })}
     </VerticalTimeline>
   );
 }
